e005b8b7a0cd78edfbd6395667ef0e441a065b00
Eric Dahlvang <v-erdahl@microsoft.com>
2017-07-13 15:54:36 -0700

    Initial Commit

---
+++ TokenBotExample/Areas/Bot/Controllers/WebChatController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TokenBotExample.Areas.Bot.Models;
+
+namespace TokenBotExample.Areas.Bot.Controllers
+{
+    public class WebChatController : Controller
+    {
+        public ActionResult Index()
+        {
+            var vm = new WebChatModel();
+            return View(vm);
+        }
+    }
+}
